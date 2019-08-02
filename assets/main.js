931858dbd90905a2b262738e2a1534d27abbaf55
prrrcl <adrianporcelnavarro@hotmail.com>
2019-08-02 16:07:19 +0200

    Edited some styles

--- assets/main.js
+++ assets/main.js
@@ -15,7 +15,20 @@ const main = () => {
     e.addEventListener('click', (event)=>{
       e.lastElementChild.classList.toggle('sub-open')
     })
-  })
+  });
+ // Appearing menu
+  TweenMax.staggerFromTo(".item-menu", 0.8,
+  {
+      alpha: 0,
+      y: -10
+  },
+  {
+      alpha: 1,
+      y: 0,
+      ease: Power3.easeOut
+  },
+  0.2);
+  
 }
 
 window.addEventListener('load', main);
